ca1396f0947709089e0befac3af1c72d666e3769
Holly Becker <becker.holly@gmail.com>
2012-09-13 23:47:39 -0700

    Alternate/more efficient implementation of hashing.

--- server.py
+++ server.py
@@ -410,6 +410,15 @@ class FilesystemService():
         """
         Returns a dictionary of filenames and their hashes known to the server.
         """
+        # from http://stackoverflow.com/questions/1131220/get-md5-hash-of-a-files-without-open-it-in-python
+        # def md5_for_file(f, block_size=2**20):
+        #     md5 = hashlib.md5()
+        #     while True:
+        #         data = f.read(block_size)
+        #         if not data:
+        #             break
+        #         md5.update(data)
+        #     return md5.hexdigest()
         hashs = [(f, hashlib.md5(open(f, 'r').read()).hexdigest())
                  for f in os.listdir('.')
                  if not os.path.isdir(f) and f[0] != '.'
